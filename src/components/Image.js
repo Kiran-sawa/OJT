e462c4b3bed603c38089f65f8d709a41904127b6
Kiran-sawa <kiransawa8@gmail.com>
2024-02-12 20:57:41 +0600

    'oki
    oki

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,19 +1,19 @@
 import React from "react";
-import raj from "../assets/raj.png";
+
 const Image = () => {
   return (
     <>
       <div className="flex flex-row">
         <div className="flex items-center justify-center h-screen mr-6">
-          <img src="h.png" height={100} width={100}></img>
+          <img src="h.png" alt="T" height={100} width={100}></img>
           <h1 className="text-center text-8xl">Thank </h1>
         </div>
         <div>
-          <img src="pic.jpg" height={500} width={500}></img>
+          <img src="pic.jpg" alt="t" height={500} width={500}></img>
         </div>
         <div className="flex items-center justify-center h-screen ml-6">
           <h1 className="text-center text-8xl">You</h1>
-          <img src="emo.jpg" height={100} width={100}></img>
+          <img src="emo.jpg" alt="r" height={100} width={100}></img>
         </div>
       </div>
     </>
