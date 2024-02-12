cbdf7ce1b54926afeb5e039cd2b6c2f22baba561
Kiran-sawa <kiransawa8@gmail.com>
2024-02-12 21:09:55 +0600

    hh

--- src/components/Image.js
+++ src/components/Image.js
@@ -2,19 +2,21 @@ import React from "react";
 
 const Image = () => {
   return (
-    <div className="flex flex-col items-center justify-center">
-      <div className="flex flex-col items-center mb-6">
-        <h1 className="text-4xl text-center md:text-8xl">Thank </h1>
-        <img src="h.png" alt="T" height={100} width={100}></img>
+    <>
+      <div className="flex flex-row">
+        <div className="flex items-center justify-center h-screen mr-6">
+          <img src="h.png" alt="T" height={100} width={100}></img>
+          <h1 className="text-center text-8xl">Thank </h1>
+        </div>
+        <div>
+          <img src="pic.jpg" alt="t" height={500} width={500}></img>
+        </div>
+        <div className="flex items-center justify-center h-screen ml-6">
+          <h1 className="text-center text-8xl">You</h1>
+          <img src="emo.jpg" alt="r" height={100} width={100}></img>
+        </div>
       </div>
-      <div className="mb-6">
-        <img src="pic.jpg" alt="t" height={300} width={300}></img>
-      </div>
-      <div className="flex flex-col items-center">
-        <h1 className="text-4xl text-center md:text-8xl">You</h1>
-        <img src="emo.jpg" alt="r" height={100} width={100}></img>
-      </div>
-    </div>
+    </>
   );
 };
 
